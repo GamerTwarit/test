cc638f017267ec2d6edc1215b4ca481be66608e4
GamerTwarit <80262192+GamerTwarit@users.noreply.github.com>
2023-09-10 12:15:01 +0530

    Update test.py

--- test.py
+++ test.py
@@ -11,9 +11,6 @@ responses = {
     "bye": "Aww, already? Okay, have a great day! \U0001F618",
 }
 
-# Define a dictionary to store context for follow-up responses
-context = {}
-
 # Function to get a response based on user input
 def get_response(user_input):
     user_input = user_input.lower()
@@ -28,20 +25,24 @@ def get_response(user_input):
     
     return response
 
+# Define a dictionary to store context for follow-up responses
+context = {}
+
 # Main conversation loop
 print("Girlfriend Bot: Hey! How's your day going? Type 'bye' when you need to go. \U0001F495")
 while True:
-    user_input = input("You: ")
-    if user_input.lower() == 'bye':
-        print("Girlfriend Bot: Aww, already? Okay, have a great day! \U0001F618")
-        break
+    user_input = input("You: ").lower()
     response = get_response(user_input)
     
-    if "how are you" in user_input:
-        if "good" in user_input or "fine" in user_input:
+    if user_input == 'bye':
+        print("Girlfriend Bot: Aww, already? Okay, have a great day! \U0001F618")
+        break
+
+    # Check for specific emotional states
+    if any(word in user_input for word in ["good", "fine", "bad", "sad", "happy"]):
+        if any(word in user_input for word in ["good", "fine"]):
             response = "That's wonderful! I'm here for you if you want to chat. \U0001F601"
-        elif "not good" in user_input or "not great" in user_input:
-            # Additional possible responses for when the user is feeling down
+        else:
             possible_responses = [
                 "I'm here to make your day better! Tell me, what's on your mind?",
                 "I'm sorry to hear that. Anything I can do to cheer you up?",
@@ -53,45 +54,21 @@ while True:
                 "I'm here to brighten your day! What's been bringing you down?",
             ]
             response = random.choice(possible_responses)
+
+    # Check for specific emotional states and provide responses
+    emotional_states = ["feeling down", "having a tough day", "feeling low", "having a bad time",
+                        "not feeling well", "feeling sad", "feeling blue", "not in a good mood",
+                        "feeling unhappy", "feeling upset", "feeling gloomy", "feeling insecure",
+                        "insecurity", "feeling lonely", "loneliness", "feeling stuck", "in a rut"]
     
-    elif "feeling down" in user_input or "having a tough day" in user_input:
-        possible_responses = [
-            "I'm here to brighten your day! What's been bothering you? Let's talk it out.",
-            "I'm here to lift your spirits! What's on your mind?",
-            "I'm here to make you smile! What's been going on?",
-            "I'm sorry to hear that. Let's chat and see if I can help improve your day.",
-        ]
-        response = random.choice(possible_responses)
-    
-    elif "feeling low" in user_input or "having a bad time" in user_input:
-        possible_responses = [
-            "I'm here to lift your spirits! What's bothering you? I'm all ears.",
-            "I'm here to make your day better! What's been weighing on you?",
-            "I'm sorry to hear that. Let's talk it out. What's been going on?",
-        ]
-        response = random.choice(possible_responses)
-    
-    elif "not feeling well" in user_input or "feeling sad" in user_input:
-        possible_responses = [
-            "I'm here to cheer you up! What's been bothering you lately? Share with me.",
-            "I'm here to brighten your mood! What's on your mind?",
-            "I'm sorry to hear that. How can I help improve your mood?",
-        ]
-        response = random.choice(possible_responses)
-    
-    elif "feeling blue" in user_input or "having a rough day" in user_input:
-        possible_responses = [
-            "I'm here to turn things around for you! What's going on that's got you down?",
-            "I'm here to provide some positivity! What's been bothering you today?",
-            "I'm here to make your day brighter! Let's talk and lift your spirits.",
-        ]
-        response = random.choice(possible_responses)
-    
-    elif "not in a good mood" in user_input or "feeling unhappy" in user_input:
-        possible_responses = [
-            "I'm here to bring some positivity into your day! What's troubling you right now?",
-            "I'm here to make you smile! Tell me, what's on your mind?",
-            "I'm here to listen and support you. What's been bothering you lately?",
-        ]
-       
-    print(response)
+    for state in emotional_states:
+        if state in user_input:
+            possible_responses = [
+                "I'm here to brighten your day! What's been bothering you? Let's talk it out.",
+                "I'm here to lift your spirits! What's on your mind?",
+                "I'm here to make you smile! What's been going on?",
+                "I'm sorry to hear that. Let's chat and see if I can help improve your day.",
+            ]
+            response = random.choice(possible_responses)
+
+    print("Girlfriend Bot:", response)
